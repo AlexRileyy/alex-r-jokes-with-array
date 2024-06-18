765f96a2af0d8a8654e850e5033c0256554b33b5
Your Name <you@example.com>
2024-06-18 14:19:25 +0100

    Changed a one liner

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,13 @@ export default function jokes() {
         <li>What did the drummer call his two daughters? Anna One, Anna Two!</li>
         <li>"Doctor Doctor! Help, I'm addicted to Twitter!" "Sorry, I'm not following you."</li>
         <li>Two monkeys were getting into the bath. One said: "Oo, oo, aah."" The other replied:"Put some cold in then."</li>
-        <li>Hedgehogs – why can’t they just share the hedge?</li>
       </ul>
   
       <h2>One Liners</h2>
       <ul>
         <li>Crime in multi-storey car parks. That is wrong on so many different levels.</li>
         <li>Conjunctivitis.com – that’s a site for sore eyes.</li>
+        <li>Hedgehogs – why can’t they just share the hedge?</li>
       </ul>
 
     </>
